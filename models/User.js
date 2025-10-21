2415b72847150eafbc05acdaee0fb659f8472bc2
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-21 20:11:50 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-export default mongoose.model("User", userSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);
