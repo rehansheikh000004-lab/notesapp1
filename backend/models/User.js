ccd05f5e922c2dd6a7d49b696c79ad6cfa125ec7
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-22 12:45:07 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model("User", userSchema);
