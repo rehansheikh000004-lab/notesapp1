5a3faac46018e7e7a327925f03a4d9798773d59b
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-27 19:01:43 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,17 +6,10 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true }
 });
 
-// ✅ Prevent double hashing
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-
-// ✅ Compare password method
-userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
