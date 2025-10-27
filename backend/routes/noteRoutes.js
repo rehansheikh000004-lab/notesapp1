bcedadd40129349740a8c3e31678ba9be96f1098
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-27 19:04:39 +0530

    Update and rename notes.js to noteRoutes.js

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -3,7 +3,7 @@ import Note from "../models/Note.js";
 
 const router = express.Router();
 
-// Get all notes
+// Get notes
 router.get("/:userId", async (req, res) => {
   const notes = await Note.find({ userId: req.params.userId });
   res.json(notes);
@@ -14,7 +14,7 @@ router.post("/", async (req, res) => {
   const { userId, title, content } = req.body;
   const note = new Note({ userId, title, content });
   await note.save();
-  res.json({ message: "Note added successfully" });
+  res.json({ message: "Note added" });
 });
 
 export default router;
