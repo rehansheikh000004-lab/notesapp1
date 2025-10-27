b43473d9e1f87d17b21c7900f58d34c7476216ee
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-27 13:33:08 +0530

    Update Note.js

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const noteSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: String,
-  content: String
+  content: String,
 });
 
-export default mongoose.models.Note || mongoose.model("Note", noteSchema);
+export default mongoose.model("Note", noteSchema);
