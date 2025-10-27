66dc3594d9d6b3a904cd56448344323d91c552eb
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-27 13:32:34 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-// Prevent OverwriteModelError when redeploying / hot reload
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-export default User;
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("User", userSchema);
